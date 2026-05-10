97cc28b7d2a05ff732c21a22f3ba2fd122c55c96
Alfred <sarifullin.alf@gmail.com>
2026-05-10 15:41:28 +0300

    feat: настроил отчистку файлов и подсчет хэша всех файлов в папке для уже об работных файлов в будущес
    — Настроил автоматическую очистку текста от лишних пробелов, переносов строк и.д
    
    — Внедрил расчет MD5-хэшей для отслеживания изменений в файлах.
    
    - Не большой рефакторинг load_file

--- src/load_data/load_file.py
+++ src/load_data/load_file.py
@@ -3,8 +3,8 @@ from .read_docx import read_docx
 from .read_pdf import read_pdf
 from .read_txt import read_txt
 
-def load_file(path):
-    ext = os.path.splitext(path)[1]
+def load_file(path: str) -> str:
+    ext = os.path.splitext(path)[1].lower()
 
     if ext == ".pdf":
         return read_pdf(path)
